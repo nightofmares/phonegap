2df6f97740a521ae8b217cd7afdbad5894cd14fc
nightofmares <amanali418@outlook.com>
2016-11-28 17:21:54 -0800

    Update apple.php

--- apple.php
+++ apple.php
@@ -1,7 +1,9 @@
-
-<php
-$candy = "candy";
-echo json_encode($candy);
-
-
+<?php
+// filename: myAjaxFile.php
+// some PHP
+    $advert = array(
+        'ajax' => 'Hello world!',
+        'advert' => $row['adverts'],
+     );
+    echo json_encode($advert);
 ?>
